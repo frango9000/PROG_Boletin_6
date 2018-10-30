f13981442ff3833c00069526f76f287afc1a36e8
Francisco Sanchez <fsancheztemprano@danielcastelao.org>
2018-10-30 12:22:18 +0100

    Boletin 6_1 format code Boletin 6_2 mejorar sout de intro de num

--- PROG_Boletin_6_2/PROG_Boletin_6_2.java
+++ PROG_Boletin_6_2/PROG_Boletin_6_2.java
@@ -16,11 +16,11 @@ public class PROG_Boletin_6_2 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        
-        System.out.println("Introduce el numero 1 :");
+
+        System.out.println("Introduce el primer numero :");
         short num1 = scan.nextShort();
 
-        System.out.println("Introduce el numero 2 :");
+        System.out.println("Introduce el segundo numero :");
         short num2 = scan.nextShort();
 
         if (num1 >= num2) {
@@ -30,5 +30,5 @@ public class PROG_Boletin_6_2 {
         System.out.println("Suma num 1 + num2 : " + (num1 + num2));
 
     }
-    
+
 }
